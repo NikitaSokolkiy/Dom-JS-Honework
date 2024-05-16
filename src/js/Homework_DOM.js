42b48fbd1331db52aa426ae160442777a8515a06
Nikita Sokol <nikitasokolskiyy@gmail.com>
2024-05-16 15:51:42 +0300

    Coment section task not finish

--- src/js/Homework_DOM.js
+++ src/js/Homework_DOM.js
@@ -13,7 +13,7 @@ function increaseCounter (){
     if (parseInt(counter.value) >= 10){
         if(!errorMessageIncrease) {
             alert("Значение не может быть больше 10!");
-            errorMessageDecrese = true;
+            errorMessageIncrease = true;
         }
     } else {
         counter.value = parseInt(counter.value) + 1;
@@ -66,3 +66,45 @@ submitButton.addEventListener("click", ()=>{
         alert("Пожалуйста, заполните все поля корректно.");
     }
 });
+
+// COMMENT SECTION TASK
+
+const titleForm = document.getElementById('title2');
+const bodyForm = document.getElementById('body2');
+const ratingForm = document.getElementById('rating2');
+const authorForm = document.getElementById('author2');
+const commitButton = document.getElementById('comment-button');
+const newArray = [];
+
+commitButton.addEventListener("click", ()=>{
+    const title = titleForm.value;
+    const body = bodyForm.value;
+    const rating = parseInt(ratingForm.value);
+    const author = authorForm.value;
+
+    if (!(title && body && !isNaN(rating) && author)){
+        if (body.length >= 55){
+            if (rating < 1 || rating >5){
+                alert(" Рейтинг должен быть в пределах от 1 до 5");
+            }
+            let bodyMin = body.slice(0, 55) + "...";
+            }
+        alert("Поля должны быть заполонены");
+        const comment = {
+            id: newArray.length + 1,
+            title,
+            body,
+            rating,
+            author
+        }
+        let newComment = document.createElement("div");
+        newComment.classList.add('comment');
+        };
+
+    let newComment = document.createElement("div");
+    newComment.classList.add('comment');
+
+
+})
+
+
